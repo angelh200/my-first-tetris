70653080688c51af1ff45e393abedc45d390ee62
Angel <angel.huamani.e@hotmail.com>
2019-04-12 04:05:34 -0500

    Initial commit

---
+++ tetris.js
@@ -0,0 +1,46 @@
+const canvas = document.querySelector('#tetris');
+const context = canvas.getContext('2d');
+
+context.scale(20, 20);
+
+const matrix = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+];
+
+function draw() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset) {
+    matrix.forEach((row, y) =>{
+        row.forEach((value, x) =>{
+            if (value !== 0) {
+                context.fillStyle = 'red';
+                context.fillRect(x + offset.x,
+                                 y + offset.y,
+                                 1, 1);
+            }
+        });
+    });
+}
+
+let lastTime = 0;
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    console.log(deltaTime);
+    draw();
+    player.pos.y = player.pos.y + 1;
+    requestAnimationFrame(update);
+}
+
+const player = {
+    pos: {x: 5, y: -1},
+    matrix: matrix
+};
+
+update();
