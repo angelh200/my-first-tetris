48848f9f1be4eef5a20dd105b8dd2907a7718ca3
Angel <angel.huamani.e@hotmail.com>
2019-05-18 08:39:01 -0500

    deleted files

--- maxScore.js
+++
@@ -1,8 +0,0 @@
-function updateMaxScore(score) {
-    let maxScore = Number(getCookie("maxScore"));
-    if(!maxScore || maxScore < score) {
-        maxScore = score;
-    }
-    setCookie("maxScore", String(maxScore));
-    document.getElementById('max-score').innerText = maxScore;
-}
