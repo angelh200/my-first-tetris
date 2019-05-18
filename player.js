74d6c614419533d2b0780a814568690142e99f98
Angel <angel.huamani.e@hotmail.com>
2019-05-18 08:32:06 -0500

    refactor createPiece

--- player.js
+++ player.js
@@ -27,7 +27,7 @@ class Player {
         sound.play();
         const pieces = 'ILJOTSZ';
         let finalScore = 0;
-        this.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+        this.matrix = this.createPiece(pieces[pieces.length * Math.random() | 0]);
         this.pos.y = 0;
         this.pos.x = (this.arena.matrix[0].length / 2 | 0) -
             (this.matrix[0].length / 2 | 0);
@@ -73,6 +73,52 @@ class Player {
                 this.pos.x = pos;
                 return;
             }
+        }
+    }
+
+    createPiece(type) {
+        if (type === 'T') {
+            return [
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 1, 0],
+            ];
+        } else if (type === 'O') {
+            return [
+                [2, 2],
+                [2, 2]
+            ];
+        } else if (type === 'L') {
+            return [
+                [0, 3, 0],
+                [0, 3, 0],
+                [0, 3, 3],
+            ];
+        } else if (type === 'J') {
+            return [
+                [0, 4, 0],
+                [0, 4, 0],
+                [4, 4, 0],
+            ];
+        } else if (type === 'I') {
+            return [
+                [0, 5, 0, 0],
+                [0, 5, 0, 0],
+                [0, 5, 0, 0],
+                [0, 5, 0, 0]
+            ];
+        } else if (type === 'S') {
+            return [
+                [0, 6, 6],
+                [6, 6, 0],
+                [0, 0, 0],
+            ];
+        } else if (type === 'Z') {
+            return [
+                [7, 7, 0],
+                [0, 7, 7],
+                [0, 0, 0],
+            ];
         }
     }
 
